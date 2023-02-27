c38fc308eaec9561d52d63928385e87923b935b4
Kennonirom <124364969+Kennonirom@users.noreply.github.com>
2023-02-27 19:34:08 +0800

    Create plurality.c

---
+++ CS50c_to_python/plurality.c
@@ -0,0 +1,41 @@
+class TestDat():
+    def __init__(self, ):
+        self.Dat1 = None
+        self.Dat2 = None
+   
+    def __str__(self):
+        return f"{self.Dat1} with {self.Dat2}"
+   
+TestArray = [] #empty array
+size = 9       #number of loops
+
+
+while True:
+    for x in range(size):  # appending empty objects
+        TestArray.append(TestDat())
+        data = input(f"data {x}: ")
+        TestArray[x].Dat1 = data
+        TestArray[x].Dat2 = 0
+        if (len(TestArray) < 2):
+            print("At Least 2 candidates")
+        elif (len(TestArray) > 9):
+            print("Maximum of 9 candidates")
+        elif(len(data) == 0):
+            break
+    if(len(data) == 0):
+        break
+
+
+numvotes =0
+voters = int(input("number of voters: "))
+for i in range(voters):
+    name = input("Vote:")
+    for j in range(len(TestArray)):
+        if (name == TestArray[j].Dat1):
+            TestArray[j].Dat2 += 1
+
+
+maxv = max(TestArray, key=lambda x: x.Dat2)
+
+
+print(f"teh winner is {maxv} votes in total")
